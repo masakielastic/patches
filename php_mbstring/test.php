7d7ca7689563339ba6f9dbf0be60f12072ee9088
Masaki Kagaya <masakielastic@gmail.com>
2013-07-29 22:04:35 +0900

    added test case

--- php_mbstring/test.php
+++ php_mbstring/test.php
@@ -1,6 +1,5 @@
 <?php
 mb_substitute_character(0xFFFD);
-
 var_dump(
     "\xEF\xBF\xBD"."\xEF\xBF\xBD" === mb_convert_encoding("\xE0\x80", 'UTF-8', 'UTF-8'),
     "\xEF\xBF\xBD"."\xEF\xBF\xBD" === mb_convert_encoding("\xED\xA0", 'UTF-8', 'UTF-8'),
